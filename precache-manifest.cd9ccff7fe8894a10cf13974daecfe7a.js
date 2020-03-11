f87201b69ab8ebc2ec3617c4278bd518506b3ab1
selvakumar <selva2242@gmail.com>
2020-03-11 22:48:20 +0530

    Updates

---
+++ precache-manifest.cd9ccff7fe8894a10cf13974daecfe7a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "befc2ffcafd6c61b3514a3a7dace3675",
+    "url": "/Blog/index.html"
+  },
+  {
+    "revision": "96180b28e882bafe1c83",
+    "url": "/Blog/static/css/main.2ead6eed.chunk.css"
+  },
+  {
+    "revision": "b3b9c0d39ba3e18fcc9b",
+    "url": "/Blog/static/js/2.d03fd81e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Blog/static/js/2.d03fd81e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96180b28e882bafe1c83",
+    "url": "/Blog/static/js/main.cc971373.chunk.js"
+  },
+  {
+    "revision": "28169be6ee094e4743ee",
+    "url": "/Blog/static/js/runtime-main.2b9520d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Blog/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "45db9494a37941dfc441fe09d4745961",
+    "url": "/Blog/static/media/post1Image.45db9494.jpg"
+  },
+  {
+    "revision": "ae0c3edb3743a410b629f2a0ef06292a",
+    "url": "/Blog/static/media/post2Image.ae0c3edb.jpg"
+  },
+  {
+    "revision": "43795c89a0ad361c75da486311fa38d6",
+    "url": "/Blog/static/media/post3Image.43795c89.jpg"
+  },
+  {
+    "revision": "d83e98c3651419e3d364228e41aa13cd",
+    "url": "/Blog/static/media/post4Image.d83e98c3.jpg"
+  },
+  {
+    "revision": "bcd18acdb9e6df1eaa7124ee790893e6",
+    "url": "/Blog/static/media/post5Image.bcd18acd.jpg"
+  }
+]);
